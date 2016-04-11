d2f40d715c1e19c1ada5c46daf4634e0191c3261
Tirupati Balan <tirupati@tirupatibalan.com>
2016-04-11 14:14:25 +0530

    Create EmiCalculator scene using clean-swift and add pod support

---
+++ EmiCalculator/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  EmiCalculator
+//
+//  Created by Tirupati Balan on 11/04/16.
+//  Copyright © 2016 CelerStudio. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+
+    }
+
+
+}
+
